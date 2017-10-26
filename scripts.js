e6ae3ff516a6bc593618392fc49bbc25def40f50
Maksym <maksymilian.makuch7@gmail.com>
2017-10-26 21:37:25 +0200

    ?

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 function getQuote() {
 	$.getJSON(quoteUrl, createTweet);
-};
+	$.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
+}
 function createTweet(input) {
 	var data = input[0];
 
@@ -12,6 +15,7 @@ function createTweet(input) {
 	if (!quoteAuthor.length) {
 		quoteAuthor = "Unknown author";
 	}
+var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 	if (tweetText.length > 140) {
 	getQuote();
 	} else {
@@ -20,7 +24,7 @@ function createTweet(input) {
 		$('.author').text("Author: " + quoteAuthor);
 		$('.tweet').attr('href', tweet);
 	}
-	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+	
 };
 $(document).ready(function() {
 	getQuote();
