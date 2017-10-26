d852c9ea1c31b0c8f583ef3e0197fdee81a29f49
Maksym <maksymilian.makuch7@gmail.com>
2017-10-26 21:23:25 +0200

    ?

--- scripts.js
+++ scripts.js
@@ -12,7 +12,6 @@ function createTweet(input) {
 	if (!quoteAuthor.length) {
 		quoteAuthor = "Unknown author";
 	}
-	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 	if (tweetText.length > 140) {
 	getQuote();
 	} else {
@@ -21,6 +20,7 @@ function createTweet(input) {
 		$('.author').text("Author: " + quoteAuthor);
 		$('.tweet').attr('href', tweet);
 	}
+	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 };
 $(document).ready(function() {
 	getQuote();
